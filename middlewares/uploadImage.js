5e03f6e6424066ee39f1661b499ea2c41f009a96
Murshidmuhammedp <murshid.mmp@gmail.com>
2024-05-08 17:07:04 +0530

    cloudinary setup

--- middlewares/uploadImage.js
+++ middlewares/uploadImage.js
@@ -1,9 +1,14 @@
 import { v2 as cloudinary } from 'cloudinary';
 import dotenv from "dotenv";
+import multer from 'multer';
 dotenv.config();
 
 cloudinary.config({
     cloud_name: process.env.CLOUD_NAME,
     api_key: process.env.API_KEY,
     api_secret: process.env.SECRET_KEY
+});
+
+const storage = multer.diskStorage({
+
 });
