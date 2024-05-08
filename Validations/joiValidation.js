5f0c07b8c21201fddb514ee69e2eaed06c42b68c
Murshidmuhammedp <murshid.mmp@gmail.com>
2024-05-08 10:19:39 +0530

    Joi validation

---
+++ Validations/joiValidation.js
@@ -0,0 +1,10 @@
+import Joi from "joi"
+
+const userjoi = Joi.object({
+    username: Joi.string(),
+    image: Joi.string(),
+    email: Joi.string(),
+    password: Joi.string()
+})
+
+export default userjoi;
