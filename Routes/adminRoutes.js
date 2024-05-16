1a5e4afb8e4293409901d93b6efa65844a667bb6
Murshidmuhammedp <murshid.mmp@gmail.com>
2024-05-16 13:15:29 +0530

    View a specific user data by ID

--- Routes/adminRoutes.js
+++ Routes/adminRoutes.js
@@ -1,7 +1,7 @@
 import express from "express";
 import { allProductView, createProduct, removeProduct, specificProduct, viewcategorywise } from "../controllers/adminProductController.js";
 import uploadImage from "../middlewares/uploadImage.js";
-import { viewalluser } from "../controllers/adminUserController.js";
+import { viewalluser, viewspecificuser } from "../controllers/adminUserController.js";
 const router = express.Router();
 
 // Product controller
@@ -15,5 +15,6 @@ router.delete('/removeproduct/:id', removeProduct);
 // User controller
 
 router.get('/usersdata',viewalluser);
+router.get('/userdata/:id',viewspecificuser);
 
 export default router;
--- controllers/adminUserController.js
+++ controllers/adminUserController.js
@@ -14,3 +14,20 @@ export const viewalluser = async (req, res, next) => {
         next(error);
     }
 };
+
+// View a specific user data by ID
+
+export const viewspecificuser = async (req, res, next) => {
+    try {
+        const id = req.params.id;
+
+        const user = await User.findById(id);
+
+        if (!user) {
+            res.status(404).json({ message: "User not found" });
+        }
+        res.status(200).json({ message: "successfully fetched user data", data: user });
+    } catch (error) {
+        next(error);
+    }
+};
